7c9e804765ecf550afd60544ccc9514d68eaf140
Sasha <sashabrava@yandex.by>
2016-11-06 19:16:17 +0300

    Graphic interface bugs fixed

--- app/src/main/java/com/cels/photofun/MainActivity.java
+++ app/src/main/java/com/cels/photofun/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cels.photofun;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
@@ -7,49 +8,78 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.PixelFormat;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TEMP_FOLDER = "photos";
+    private static final String TEMP_FILE = "temp.jpg";
     private static final int PICK_IMAGE = 2;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-
-   /* static {
-        System.loadLibrary("startJNI");
-    }*/
-
+    private static final int ROTATE_DEGREE = 90;
+    private static final int JPEG_QUALITY = 100;
     static {
         System.loadLibrary("buildSegmented");
     }
 
+    private Bitmap imageForC;
     private Bitmap imageBitmap;
     private SeekBar seekBar;
+    private boolean newImage = true;
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "I rotated", Toast.LENGTH_LONG).show();
         start();
+        if (imageForC != null) {
+            ImageView photoImg = (ImageView) findViewById(R.id.imageView);
+            photoImg.setImageBitmap(imageForC);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                android.os.Process.killProcess(android.os.Process.myPid());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void start() {
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(android.R.drawable.ic_lock_power_off);
+        }
         seekBar = (SeekBar) findViewById(R.id.seekBar2);
-
+        ImageView photoImg = (ImageView) findViewById(R.id.imageView);
+        photoImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageRotate();
+            }
+        });
         getWindow().setFormat(PixelFormat.RGBA_8888);
         if (imageBitmap != null) {
-            ImageView photoImg = (ImageView) findViewById(R.id.imageView);
             photoImg.setImageBitmap(imageBitmap);
         }
         Button photo = (Button) findViewById(R.id.buttonTakePic);
@@ -57,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
+                newImage = true;
             }
         });
         Button segmentation = (Button) findViewById(R.id.buttonSetSegmentation);
@@ -64,15 +95,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (imageBitmap != null) {
-                    Bitmap imageForC = Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth(), imageBitmap.getHeight(), false);
-                    buildSegmented(imageForC, seekBar.getProgress() + 1);
-                    ImageView photo = (ImageView) findViewById(R.id.imageView);
-                    //imageBitmap
-                    /*KMeans kMeans = new KMeans(imageBitmap, seekBar.getProgress() + 1);
-                    Toast.makeText(getApplicationContext(), "" + (seekBar.getProgress() + 1), Toast.LENGTH_LONG).show();
-                    imageBitmap = kMeans.getResult();*/
-                    photo.setImageBitmap(imageForC);
-                    Toast.makeText(getApplicationContext(), "Successful segmentation", Toast.LENGTH_LONG).show();
+                    imageForC = Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth(), imageBitmap.getHeight(), false);
+                    MyTask myTask = new MyTask(imageForC);
+                    myTask.execute();
                 } else {
                     Toast.makeText(getApplicationContext(), "Please choose the image", Toast.LENGTH_LONG).show();
                 }
@@ -88,6 +113,14 @@ public class MainActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+                newImage = true;
+            }
+        });
+        Button save = (Button) findViewById(R.id.buttonSave);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImageFile();
             }
         });
     }
@@ -97,17 +130,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         start();
-        //  Toast.makeText(getApplicationContext(), startNDI(), Toast.LENGTH_LONG).show();
-
-
-
     }
 
     private void dispatchTakePictureIntent() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        File f = new File(getApplicationContext().getExternalFilesDir("photos"), "temp.jpg");
-
+        File f = new File(getApplicationContext().getExternalFilesDir(TEMP_FOLDER), TEMP_FILE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
         startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
     }
@@ -115,19 +142,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            File f = new File(getApplicationContext().getExternalFilesDir("photos"), "temp.jpg");
+            File f = new File(getApplicationContext().getExternalFilesDir(TEMP_FOLDER), TEMP_FILE);
             ImageView photoImg = (ImageView) findViewById(R.id.imageView);
             BitmapFactory.Options options = new BitmapFactory.Options();
             options.inPreferredConfig = Bitmap.Config.ARGB_8888;
             try {
                 imageBitmap = BitmapFactory.decodeStream(new FileInputStream(f));
                 if (photoImg.getWidth() > 0 && photoImg.getHeight() > 0) {
-                    // imageBitmap = Bitmap.createScaledBitmap(imageBitmap, photoImg.getWidth(), photoImg.getHeight(), false);
-                    // photoImg.setImageBitmap(imageBitmap);
                     addImage();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Please try again, but don't turn the screen.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Please try again. Error has occured", Toast.LENGTH_LONG).show();
                 }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -136,18 +160,49 @@ public class MainActivity extends AppCompatActivity {
 
         } else if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             try {
-                //ImageView photoImg = (ImageView) findViewById(R.id.imageView);
                 InputStream inputStream = getApplicationContext().getContentResolver().openInputStream(data.getData());
                 BitmapFactory.Options options = new BitmapFactory.Options();
                 options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-
                 imageBitmap = BitmapFactory.decodeStream(inputStream);
                 addImage();
-                // imageBitmap = Bitmap.createScaledBitmap(imageBitmap, photoImg.getWidth(), photoImg.getHeight(), false);
-                // photoImg.setImageBitmap(imageBitmap);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void saveImageFile() {
+        if (imageForC != null) {
+            String timeStamp = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault()).format(System.currentTimeMillis());
+            File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/PhotoFun/");
+            if (!storageDir.exists())
+                storageDir.mkdirs();
+            File image = null;
+            try {
+                image = new File(storageDir, "Fun" + timeStamp + ".jpeg");
+                if (!image.exists()) {
+                    Log.w("File", "new file created");
+                    image.createNewFile();
+                }
+                Bitmap bitmap = imageForC;
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, JPEG_QUALITY, bos);
+                byte[] bitmapData = bos.toByteArray();
+                FileOutputStream fos = new FileOutputStream(image);
+                fos.write(bitmapData);
+                fos.flush();
+                fos.close();
+                Toast.makeText(getApplicationContext(), "Successfully saved to DCIM/PhotoFun", Toast.LENGTH_LONG).show();
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                values.put(MediaStore.MediaColumns.DATA, image.getPath());
+                getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "There is no segmented image", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -157,33 +212,61 @@ public class MainActivity extends AppCompatActivity {
         int coefWidth = imageBitmap.getWidth() / photoImg.getWidth();
         int coefHeight = imageBitmap.getHeight() / photoImg.getHeight();
         int coefScale = Math.min(coefWidth, coefHeight);
-
         imageBitmap = Bitmap.createScaledBitmap(imageBitmap, imageBitmap.getWidth() / coefScale, imageBitmap.getHeight() / coefScale, false);
         photoImg.setImageBitmap(imageBitmap);
-        photoImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageRotate();
-            }
-        });
+
     }
 
     private void imageRotate() {
         if (imageBitmap == null) return;
+        else if (imageForC != null && !newImage) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(ROTATE_DEGREE);
+            imageForC = Bitmap.createBitmap(imageForC, 0, 0, imageForC.getWidth(), imageForC.getHeight(), matrix, true);
+            ImageView photoImg = (ImageView) findViewById(R.id.imageView);
+            photoImg.setImageBitmap(imageForC);
+        } else {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(ROTATE_DEGREE);
+            imageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
+            ImageView photoImg = (ImageView) findViewById(R.id.imageView);
+            photoImg.setImageBitmap(imageBitmap);
 
-        Matrix matrix = new Matrix();
-        matrix.postRotate(90);
-        imageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
-        ImageView photoImg = (ImageView) findViewById(R.id.imageView);
-        photoImg.setImageBitmap(imageBitmap);
-
-
+        }
     }
 
     public native Bitmap buildSegmented(Bitmap bitmap, int clusterCount);
-   /* public native String startNDI();
-    static{
-        System.loadLibrary("startNDI");
-    }*/
 
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        private final Bitmap imageForC;
+
+        MyTask(Bitmap imageForC) {
+            this.imageForC = imageForC;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Button segment = (Button) findViewById(R.id.buttonSetSegmentation);
+            segment.setText(getApplicationContext().getString(R.string.segment_process));
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            buildSegmented(imageForC, seekBar.getProgress() + 1);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            ImageView photo = (ImageView) findViewById(R.id.imageView);
+            photo.setImageBitmap(imageForC);
+            Toast.makeText(getApplicationContext(), "Successful segmentation", Toast.LENGTH_LONG).show();
+            Button segment = (Button) findViewById(R.id.buttonSetSegmentation);
+            segment.setText(getApplicationContext().getString(R.string.segment));
+            newImage = false;
+        }
+    }
 }
--- app/src/main/jni/buildSegmented.c
+++ app/src/main/jni/buildSegmented.c
@@ -29,7 +29,7 @@ void createClusters(Cluster **clusters, int amount){
   LOGD("big memory got");
  *clusters = x;
   LOGD("value is gotten");
-//*clusters = (Cluster*) malloc(sizeof(Cluster)*amount);
+
 }
 void cluster_new(Cluster *cluster)
 {
@@ -45,9 +45,7 @@ int getRGB(Cluster *cluster) {
            int r = (int) (cluster->reds / cluster->pixelCount);
            int g = (int) (cluster->greens / cluster->pixelCount);
            int b = (int) (cluster->blues /cluster->pixelCount);
-            //LOGD("red:%d green:%d blue:%d", r, g, b);
            return 0xff000000 | r << 16 | g << 8 | b;
-           //return r+g+b;
        }
 void addPixel(Cluster *cluster, int pixel) {
 
@@ -108,17 +106,7 @@ int findMinimalCluster(Cluster clusters[], int amountClusters, int rgb) {
       LOGE("Bitmap format is not RGBA_8888!");
       return NULL;
       }
-    /*  Cluster cluster;
-      cluster_with_id(&cluster, amountClusters);
-      LOGD("Created cluster:%d", amountClusters);
-      LOGD("Id %d", cluster.id);
-      return NULL;*/
-  //read pixels of bitmap into native memory :
-  //
-  //NOW tempPixels is correct array of image
- // uint32_t pixel = tempPixels[info.width * y + x]; // y = height
- //Cluster **clusters;
- //createClusters(clusters, amountClusters);
+
  Cluster clusters[20];
  LOGD("clusters created");
  for (int i =0; i< amountClusters; i++)
@@ -149,8 +137,6 @@ int findMinimalCluster(Cluster clusters[], int amountClusters, int rgb) {
               {//1st color to clusters
                    uint32_t pixel = tempPixels[info.width *currentY + currentX];
                    addPixel(&clusters[i], (int)pixel);
-                   LOGD("Id %d", clusters[i].id);
-                   LOGD("Color_red %d", clusters[i].red);
                    currentX += differenceX;
                    currentY += differenceY;
              }
@@ -168,15 +154,12 @@ int findMinimalCluster(Cluster clusters[], int amountClusters, int rgb) {
                         for (int x = 0; x < info.width; x++) {
                             int pixel = (int)tempPixels[info.width *y + x];
                             int clusterMinDistance = findMinimalCluster(clusters, amountClusters, pixel);
-                            //Cluster cluster = findMinimalCluster(clusters, pixel);
                             if (idArray[x + info.width * y] != clusterMinDistance) {
 
                                 if (idArray[x + info.width * y] != -1) {
                                     removePixel( &clusters[idArray[x + info.width * y]], pixel);
-                                   // clusters[idArray[x + width * y]].removePixel(pixel);
                                 }
                                 addPixel( &clusters[clusterMinDistance], pixel);
-                                //cluster.addPixel(pixel);
                                 pixelChangedCluster = true;
                                 idArray[x + info.width * y] = clusterMinDistance;
                             }
@@ -184,25 +167,12 @@ int findMinimalCluster(Cluster clusters[], int amountClusters, int rgb) {
                     }
 
                 }
-
- LOGD("Dude, everything is OK");
-LOGD("creating new bitmap...");
- /* jclass bitmapCls = (*env)->GetObjectClass(env,bitmap);
- jmethodID createBitmapFunction = (*env)->GetStaticMethodID(env,bitmapCls, "createBitmap", "(IILandroid/graphics/Bitmap$Config;)Landroid/graphics/Bitmap;");
-  jstring configName = (*env)->NewStringUTF(env,"ARGB_8888");
-  jclass bitmapConfigClass = (*env)->FindClass(env,"android/graphics/Bitmap$Config");
-  jmethodID valueOfBitmapConfigFunction = (*env)->GetStaticMethodID(env,bitmapConfigClass, "valueOf", "(Ljava/lang/String;)Landroid/graphics/Bitmap$Config;");
-  jobject bitmapConfig = (*env)->CallStaticObjectMethod(env,bitmapConfigClass, valueOfBitmapConfigFunction, configName);
-  jobject newBitmap = (*env)->CallStaticObjectMethod(env,bitmapCls, createBitmapFunction, info.width, info.height, bitmapConfig);
-
-    uint32_t* newBitmapPixels = (uint32_t*) bitmapPixels;
-
-     if ((ret = AndroidBitmap_lockPixels(env, newBitmap, &bitmapPixels)) < 0)
+LOGD("editing old bitmap...");
+   if ((ret = AndroidBitmap_lockPixels(env, bitmap, &bitmapPixels)) < 0)
        {
-       LOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+       LOGE("AndroidBitmap_lockPixels() failed while writing to file ! error=%d", ret);
        return NULL;
        }
-*/
 
     for (int x = 0; x < info.width; x++)
       for (int y = 0; y < info.height; y++)
@@ -210,10 +180,10 @@ LOGD("creating new bitmap...");
         uint32_t pixel = tempPixels[info.width * y + x];
         src[info.width * y + x] = getRGB(&clusters[idArray[info.width * y + x]]);
         }
-         for (int i = 0; i< amountClusters; i++)
-         LOGE("Pixel_colour_red %d", getRGB(&clusters[i]));
+         AndroidBitmap_unlockPixels(env, bitmap);
 
-//AndroidBitmap_unlockPixels(env, newBitmap);
-//return newBitmap;
+free(idArray);
+free(clusters);
+free(tempPixels);
 return NULL;
    }
