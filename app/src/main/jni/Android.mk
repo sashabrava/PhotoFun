82cdb009d557cc0d2098e888ab3591af4fd123da
Alexandr <sashabrava@yandex.by>
2019-06-16 15:16:08 +0300

    Fixed project for modern software versions

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,12 +3,10 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := buildSegmented
 LOCAL_LDFLAGS := -Wl,--build-id,-ljnigraphics
-LOCAL_SRC_FILES := \
-	D:\java\PhotoFun\app\src\main\jni\buildSegmented.c \
-LOCAL_SHARED_LIBRARIES := libutils libcutils
+LOCAL_SRC_FILES :=  $(LOCAL_PATH)\buildSegmented.c
+LOCAL_LDLIBS += -lcutils -lutils
 
-LOCAL_LDLIBS := -llog 
-LOCAL_C_INCLUDES += D:\java\PhotoFun\app\src\main\jni
-LOCAL_C_INCLUDES += D:\java\PhotoFun\app\src\debug\jni
+LOCAL_LDLIBS := -llog
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_CFLAGS := -g
 include $(BUILD_SHARED_LIBRARY)
